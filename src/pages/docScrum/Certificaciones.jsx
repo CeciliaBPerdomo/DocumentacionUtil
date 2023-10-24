2dbdf8d56d8d61b8ec1105ee2d7d34c6d1239ef0
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2023-10-24 18:45:12 -0300

    Responsive SCRUM

--- src/pages/docScrum/Certificaciones.jsx
+++ src/pages/docScrum/Certificaciones.jsx
@@ -32,8 +32,8 @@ function Certificaciones() {
         <div className="container text-center">
             <div className="row">
                 {certificaciones.map((item, id) => (
-                    <div className="col-4" key={id}>
-                        <div className={"alert alert-" + item.color} role="alert" style={{height: "55px", color: "black" }}>
+                    <div className="col-lg-4 col-sm-1 col-md-1" key={id}>
+                        <div className={"alert alert-" + item.color} role="alert" style={{minHeight: "55px", color: "black" }}>
                             <Link to={item.web} style={{ textDecoration: "none", color: "black" }}>
                                 <p>  {item.descripcion} </p>
                             </Link>
--- src/pages/docScrum/Documentacion.jsx
+++ src/pages/docScrum/Documentacion.jsx
@@ -45,8 +45,8 @@ function Documentacion() {
         <div className="container text-center">
             <div className="row">
                 {documentacion.map((item, id) => (
-                    <div className="col-4" key={id}>
-                        <div className={"alert alert-" + item.color} role="alert" style={{height: "75px", color: "black" }}>
+                    <div className="col-lg-4 col-sm-1 col-md-1" key={id}>
+                        <div className={"alert alert-" + item.color} role="alert" style={{maxHeight: "75px", color: "black" }}>
                             <Link to={item.web} style={{ textDecoration: "none", color: "black" }}>
                                 <p>  {item.descripcion} </p>
                             </Link>
